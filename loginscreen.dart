2bf1f7caf65e7e55d07723ba8235f4aaba482a59
nazirahramli <56263274+nazirahramli@users.noreply.github.com>
2020-03-15 23:44:02 +0800

    Add files via upload

---
+++ loginscreen.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:bakeryforyou/registerscreen.dart';
+
+void main() => runApp(LoginScreen());
+bool rememberMe = false;
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  double screenHeight;
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      title: 'Material App',
+      home: Scaffold(
+          resizeToAvoidBottomPadding: false,
+          body: Stack(
+            children: <Widget>[
+              upperHalf(context),
+              lowerHalf(context),
+              pageTitle(),
+            ],
+          )),
+    );
+  }
+
+  Widget upperHalf(BuildContext context) {
+    return Container(
+      height: screenHeight / 2,
+      child: Image.asset(
+        'assets/images/login.jpg',
+        fit: BoxFit.cover,
+        width: 500,
+      ),
+    );
+  }
+
+  Widget lowerHalf(BuildContext context) {
+    return Container(
+      height: 320,
+      margin: EdgeInsets.only(top: screenHeight / 3),
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: Column(
+        children: <Widget>[
+          Card(
+            elevation: 10,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  TextField(
+                      controller: null,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email),
+                      )),
+                  TextField(
+                    controller: null,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      icon: Icon(Icons.lock),
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: _forgotPassword,
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                      ),
+                      MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        minWidth: 100,
+                        height: 50,
+                        child: Text('Login'),
+                        color: Colors.brown,
+                        textColor: Colors.white,
+                        elevation: 10,
+                        onPressed: _userLogin,
+                      ),
+                    ],
+                  ),
+                  CheckboxListTile(
+                    title: Text("Remember Me"),
+                    value: rememberMe,
+                    onChanged: _onRememberMeChanged,
+                    controlAffinity: ListTileControlAffinity
+                        .leading, //  <-- leading Checkbox
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Don't have an account? " , style: TextStyle(fontSize: 16.0)),
+              GestureDetector(
+                onTap: _registerUser,
+                child: Text(
+                  "Create Account",
+                  style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget pageTitle() {
+    return Container(
+      //color: Color.fromRGBO(255, 200, 200, 200),
+      margin: EdgeInsets.only(top: 60),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.shopping_basket,
+            size: 40,
+            color: Colors.white,
+          ),
+          Text(
+            "bakeryforyou",
+            style: TextStyle(
+                fontSize: 36, color: Colors.white, fontWeight: FontWeight.w900),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _userLogin() {}
+
+  void _registerUser() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) => RegisterScreen()));
+  }
+
+  void _forgotPassword() {
+    TextEditingController phoneController = TextEditingController();
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Forgot Password?"),
+          content: new Container(
+            height: 100,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Enter your recovery email",
+                ),
+                TextField(
+                    decoration: InputDecoration(
+                  labelText: 'Email',
+                  icon: Icon(Icons.email),
+                ))
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Yes"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                print(
+                  phoneController.text,
+                );
+              },
+            ),
+            new FlatButton(
+              child: new Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _onRememberMeChanged(bool newValue) => setState(() {
+        rememberMe = newValue;
+        print(rememberMe);
+        if (rememberMe) {
+        } else {}
+      });
+}
